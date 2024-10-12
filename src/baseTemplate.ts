021d3dced79d36fb81b7738907167e70b2201770
Rick Henry <rickhenry@rickhenry.dev>
2024-10-12 10:55:15 -0400

    add card cloning
    Signed-off-by: Rick Henry <rickhenry@rickhenry.dev>

--- src/baseTemplate.ts
+++ src/baseTemplate.ts
@@ -8,7 +8,7 @@ export type Card = {
   index: string;
 };
 
-export function defaultCard() {
+export function blankCard() {
   return {
     title: "",
     corner: "",
@@ -17,187 +17,152 @@ export function defaultCard() {
     index: "",
   };
 }
+// TODO: display page numbers
+// TODO: manually add pages
+// TODO:: show front and back
 
 export function defaultCardList() {
   const cards = [];
   for (let i = 0; i < 10; i++) {
-    cards.push(defaultCard());
+    cards.push(blankCard());
   }
   return cards;
 }
-// TODO: card buttons div + class, add duplicate and delete buttons
+
+Handlebars.registerHelper("date", (date: Date) => {
+  return `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
+});
+
+Handlebars.registerPartial(
+  "cardFront",
+  `
+<div class="card">
+  <div class="card-buttons">
+    <button class="edit-card-button noprint" data-card-idx="{{ cidx }}" data-page-number="{{ pidx }}">edit</button>
+    <button class="dup-card-button noprint" data-card-text="{{ front }}" data-card-idx="{{ cidx }}" data-page-number="{{ pidx }}">clone</button>
+    <button class="del-card-button noprint" data-card-text="{{ front }}" data-card-idx="{{ cidx }}" data-page-number="{{ pidx }}">erase</button>
+  </div>
+  <p class="main">{{ front }}</p>
+  <h2 class="title">{{ title }}</h2>
+  <div class="corner">{{ corner }}</div>
+</div>
+`,
+);
+
+Handlebars.registerPartial(
+  "cardBack",
+  `
+<div class="card">
+  <div class="card-buttons">
+    <button class="edit-card-button noprint" data-card-idx="{{ cidx }}" data-page-number="{{ pidx }}">edit</button>
+    <button class="dup-card-button noprint" data-card-idx="{{ cidx }}" data-page-number="{{ pidx }}">clone</button>
+    <button class="del-card-button noprint" data-card-idx="{{ cidx }}" data-page-number="{{ pidx }}">delete</button>
+  </div>
+  <p class="main">{{ back }}</p>
+  <div class="corner">{{ index }}</div>
+</div>
+`,
+);
 
 export default Handlebars.compile(`
 {{#each pages}}
-      <div class="around">
-        <button class="removepage noprint" data-page-number="{{@index}}">Delete Page</button>
-        <div class="cards front">
-          <!-- margin -->
-          <div class="left-marks"></div>
-          <div class="vert-marks"></div>
-          <div></div>
-          <div class="right-marks"></div>
+  <div class="around">
+    <button class="removepage noprint" data-page-number="{{@index}}">Delete Page</button>
+    <div class="cards front">
+      <!-- margin -->
+      <div class="left-marks"></div>
+      <div class="vert-marks"></div>
+      <div></div>
+      <div class="right-marks"></div>
 
-          <!-- row 1 -->
-          <div class="left-marks"></div>
-          <div class="card">
-            <button class="edit-card-button noprint" data-card-idx="0" data-page-number="{{@index}}">Edit</button>
-            <p class="main">{{ this.0.front }}</p>
-            <h2 class="title">{{ this.0.title }}</h2>
-            <div class="corner">{{ this.0.corner }}</div>
-          </div>
-          <div class="card">
+      <!-- row 1 -->
+      <div class="left-marks"></div>
+      {{> cardFront cidx=0 pidx=@index front=this.0.front title=this.0.title corner=this.0.corner}}
+      {{> cardFront cidx=1 pidx=@index front=this.1.front title=this.1.title corner=this.1.corner}}
+      <div class="right-marks"></div>
 
-<button class="edit-card-button noprint" data-card-idx="1" data-page-number="{{@index}}">Edit</button>
-            <p class="main">{{ this.1.front }}</p>
-            <h2 class="title">{{ this.1.title }}</h2>
-            <div class="corner">{{ this.1.corner }}</div>
-          </div>
-          <div class="right-marks"></div>
+      <!-- row 2 -->
+      <div class="left-marks"></div>
+      {{> cardFront cidx=2 pidx=@index front=this.2.front title=this.2.title corner=this.2.corner}}
+      {{> cardFront cidx=3 pidx=@index front=this.3.front title=this.3.title corner=this.3.corner}}
+      <div class="right-marks"></div>
 
-          <!-- row 2 -->
-          <div class="left-marks"></div>
-          <div class="card">
-<button class="edit-card-button noprint" data-card-idx="2" data-page-number="{{@index}}">Edit</button>
-            <p class="main">{{ this.2.front }}</p>
-            <h2 class="title">{{ this.2.title }}</h2>
-            <div class="corner">{{ this.2.corner }}</div>
-          </div>
-          <div class="card">
-<button class="edit-card-button noprint" data-card-idx="3" data-page-number="{{@index}}">Edit</button>
-            <p class="main">{{ this.3.front }}</p>
-            <h2 class="title">{{ this.3.title }}</h2>
-            <div class="corner">{{ this.3.corner }}</div>
-          </div>
-          <div class="right-marks"></div>
+      <!-- row 3 -->
+      <div class="left-marks"></div>
+      {{> cardFront cidx=4 pidx=@index front=this.4.front title=this.4.title corner=this.4.corner}}
+      {{> cardFront cidx=5 pidx=@index front=this.5.front title=this.5.title corner=this.5.corner}}
+      <div class="right-marks"></div>
 
-          <!-- row 3 -->
-          <div class="left-marks"></div>
-          <div class="card">
-            <button class="edit-card-button noprint" data-card-idx="4" data-page-number="{{@index}}">Edit</button>
-            <p class="main">{{ this.4.front }}</p>
-            <h2 class="title">{{ this.4.title }}</h2>
-            <div class="corner">{{ this.4.corner }}</div>
-          </div>
-          <div class="card">
+      <!-- row 4 -->
+      <div class="left-marks"></div>
+      {{> cardFront cidx=6 pidx=@index front=this.6.front title=this.6.title corner=this.6.corner}}
+      {{> cardFront cidx=7 pidx=@index front=this.7.front title=this.7.title corner=this.7.corner}}
+      <div class="right-marks"></div>
 
-            <button class="edit-card-button noprint" data-card-idx="5" data-page-number="{{@index}}">Edit</button>
-            <p class="main">{{ this.5.front }}</p>
-            <h2 class="title">{{ this.5.title }}</h2>
-            <div class="corner">{{ this.5.corner }}</div>
-          </div>
-          <div class="right-marks"></div>
+      <!-- row -->
+      <div class="left-marks"></div>
+      {{> cardFront cidx=8 pidx=@index front=this.8.front title=this.8.title corner=this.8.corner}}
+      {{> cardFront cidx=9 pidx=@index front=this.9.front title=this.9.title corner=this.9.corner}}
+      <div class="right-marks"></div>
 
-          <!-- row 4 -->
-          <div class="left-marks"></div>
-          <div class="card">
+      <!-- margin -->
+      <div></div>
+      <div class="vert-marks"></div>
+      <div class=""></div>
+      <div></div>
+    </div>
+  </div>
 
-            <button class="edit-card-button noprint" data-card-idx="6" data-page-number="{{@index}}">Edit</button>
-            <p class="main">{{ this.6.front }}</p>
-            <h2 class="title">{{ this.6.title }}</h2>
-            <div class="corner">{{ this.6.corner }}</div>
-          </div>
-          <div class="card">
+  <div class="around back">
+    <div class="cards">
+      <!-- margin -->
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
 
-            <button class="edit-card-button noprint" data-card-idx="7" data-page-number="{{@index}}">Edit</button>
-            <p class="main">{{ this.7.front }}</p>
-            <h2 class="title">{{ this.7.title }}</h2>
-            <div class="corner">{{ this.7.corner }}</div>
-          </div>
-          <div class="right-marks"></div>
+      <!-- row 1 -->
+      <div></div>
+      {{> cardBack cidx=0 pidx=@index back=this.0.back index=this.0.index }}
+      {{> cardBack cidx=1 pidx=@index back=this.1.back index=this.1.index }}
+      <div></div>
 
-          <!-- row -->
-          <div class="left-marks"></div>
-          <div class="card">
-            <button class="edit-card-button noprint" data-card-idx="8" data-page-number="{{@index}}">Edit</button>
-            <p class="main">{{ this.8.front }}</p>
-            <h2 class="title">{{ this.8.title }}</h2>
-            <div class="corner">{{ this.8.corner }}</div>
-          </div>
-          <div class="card">
-            <button class="edit-card-button noprint" data-card-idx="9" data-page-number="{{@index}}">Edit</button>
-            <p class="main">{{ this.9.front }}</p>
-            <h2 class="title">{{ this.9.title }}</h2>
-            <div class="corner">{{ this.9.corner }}</div>
-          </div>
-          <div class="right-marks"></div>
+      <!-- row 2 -->
+      <div></div>
+      {{> cardBack cidx=2 pidx=@index back=this.2.back index=this.2.index }}
+      {{> cardBack cidx=3 pidx=@index back=this.3.back index=this.3.index }}
+      <div></div>
 
-          <!-- margin -->
-          <div></div>
-          <div class="vert-marks"></div>
-          <div class=""></div>
-          <div></div>
-        </div>
-      </div>
+      <!-- row 3 -->
+      <div></div>
+      {{> cardBack cidx=4 pidx=@index back=this.4.back index=this.4.index }}
+      {{> cardBack cidx=5 pidx=@index back=this.5.back index=this.5.index }}
+      <div></div>
 
-      <div class="around back">
-        <div class="cards">
-          <!-- margin -->
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
+      <!-- row 4 -->
+      <div></div>
+      {{> cardBack cidx=6 pidx=@index back=this.6.back index=this.6.index }}
+      {{> cardBack cidx=7 pidx=@index back=this.7.back index=this.7.index }}
+      <div></div>
 
-          <!-- row 1 -->
-          <div></div>
-          <div class="card">
-            <p class="main">{{ this.0.back }}</p>
-            <div class="corner">{{ this.0.index }}</div>
-          </div>
-          <div class="card">
-            <p class="main">{{ this.1.back }}</p>
-            <div class="corner">{{ this.1.index }}</div>
-          </div>
-          <div></div>
-
-          <!-- row 2 -->
-          <div></div>
-          <div class="card">
-            <p class="main">{{ this.2.back }}</p>
-            <div class="corner">{{ this.2.index }}</div>
-          </div>
-          <div class="card">
-            <p class="main">{{ this.3.back }}</p>
-            <div class="corner">{{ this.3.index }}</div>
-          </div>
-          <div></div>
-
-          <!-- row 3 -->
-          <div></div>
-          <div class="card">
-            <p class="main">{{ this.4.back }}</p>
-            <div class="corner">{{ this.4.index }}</div>
-          </div>
-          <div class="card">
-            <p class="main">{{ this.5.back }}</p>
-            <div class="corner">{{ this.5.index }}</div>
-          </div>
-          <div></div>
-
-          <!-- row 4 -->
-          <div></div>
-          <div class="card">
-            <p class="main">{{ this.6.back }}</p>
-            <div class="corner">{{ this.6.index }}</div>
-          </div>
-          <div class="card">
-            <p class="main">{{ this.7.back }}</p>
-            <div class="corner">{{ this.7.index }}</div>
-          </div>
-          <div></div>
-
-          <!-- row 5 -->
-          <div></div>
-          <div class="card">
-            <p class="main">{{ this.8.back }}</p>
-            <div class="corner">{{ this.8.index }}</div>
-          </div>
-          <div class="card">
-            <p class="main">{{ this.9.back }}</p>
-            <div class="corner">{{ this.9.index }}</div>
-          </div>
-          <div></div>
-        </div>
-      </div>
+      <!-- row 5 -->
+      <div></div>
+      {{> cardBack cidx=8 pidx=@index back=this.8.back index=this.8.index }}
+      {{> cardBack cidx=9 pidx=@index back=this.9.back index=this.9.index }}
+      <div></div>
+    </div>
+  </div>
 {{/each}}
+`);
+
+export const loadTableTemplate = Handlebars.compile(`
+{{#each index}}
+<tr>
+  <td>{{this.name}}</td>
+  <td>{{ date this.date }}</td>
+  <td><button class="load-item-button" data-uniqueid="{{ this.uniqueid }}">load</button></td>
+  <td><button class="delete-item-button" data-uniqueid="{{ this.uniqueid }}">delete</button></td>
+</tr>
+{{/each}}
+
 `);
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,148 @@
 import "./style.css";
-import template, { type Card, defaultCardList } from "./baseTemplate";
+import template, {
+  type Card,
+  blankCard,
+  defaultCardList,
+  loadTableTemplate,
+} from "./baseTemplate";
+import Swal from "sweetalert2";
 
-const pages: Card[][] = [];
+// TODO: delete saved cards button
+
+let pages: Card[][] = [];
 let currentPage = 0;
 let currentCard = 0;
 let endCard = 0;
 let endPage = 0;
 
 function showError(message: string) {
-  alert(`ERROR: ${message}`);
+  Swal.fire({
+    icon: "error",
+    text: `ERROR: ${message}`,
+  });
+}
+
+// serializeds pages array to json and downloads as a file
+function download() {
+  const json = JSON.stringify(pages);
+  const blob = new Blob([json], { type: "application/json" });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "cards.json";
+  a.click();
+  URL.revokeObjectURL(url);
+}
+
+type SavedIndexItem = {
+  name: string;
+  date: Date;
+  uniqueid: string;
+};
+
+// Saving to the browser local storage.
+// Local storage item "index" will be an array of SavedIndexItem, containing the unique id.
+// The actual data is json stored with the unique id from the index.
+
+function saveToBrowser() {
+  const index: SavedIndexItem[] = JSON.parse(
+    localStorage.getItem("index") ?? "[]",
+  );
+  const name = (document.getElementById("save-name") as HTMLInputElement).value;
+  const uniqueid = "id" + (Math.random() + 1).toString(16).slice(2);
+  index.push({
+    name,
+    date: new Date(),
+    uniqueid,
+  });
+  localStorage.setItem("index", JSON.stringify(index));
+  localStorage.setItem(uniqueid, JSON.stringify(pages));
+  Swal.fire({
+    icon: "success",
+    text: "Saved",
+  });
+}
+
+function loadFromStorage(uniqueid: string) {
+  Swal.fire({
+    text: `loading cards will delete all current cards. are you sure?`,
+    title: "confirm load",
+    icon: "warning",
+    showCancelButton: true,
+    cancelButtonText: "no, cancel",
+    cancelButtonColor: "#3085d6",
+    confirmButtonColor: "green",
+    confirmButtonText: "yes, load",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      const loadedPages: Card[][] = JSON.parse(
+        localStorage.getItem(uniqueid) ?? "[]",
+      );
+      pages = loadedPages;
+      endPage = pages.length - 1;
+      for (let i = 0; i < pages[endPage].length; i++) {
+        if (cardIsBlank(pages[endPage][i])) {
+          endCard = i - 1;
+          break;
+        }
+      }
+    }
+    render();
+  });
+}
+
+function loadFromFile() {
+  const fileInput = document.getElementById(
+    "load-upload-file",
+  ) as HTMLInputElement;
+  if (fileInput.files?.length !== 1) {
+    showError("no file selected");
+    return;
+  }
+  const reader = new FileReader();
+  reader.onload = (e) => {
+    const json = e.target?.result;
+    if (typeof json !== "string") {
+      return;
+    }
+    try {
+      const loadedPages = JSON.parse(json);
+      if (!Array.isArray(loadedPages) || loadedPages.length === 0) {
+        showError("invalid json");
+        return;
+      }
+      if (
+        !("title" in loadedPages[0]) ||
+        !("front" in loadedPages[0]) ||
+        !("back" in loadedPages[0]) ||
+        !("corner" in loadedPages[0]) ||
+        !("index" in loadedPages[0])
+      ) {
+      }
+
+      closeLoad();
+      Swal.fire({
+        text: `loading cards will delete all current cards. are you sure?`,
+        title: "confirm load",
+        icon: "warning",
+        showCancelButton: true,
+        cancelButtonText: "no, cancel",
+        cancelButtonColor: "#3085d6",
+        confirmButtonColor: "#green",
+        confirmButtonText: "yes, load",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          pages = loadedPages;
+          render();
+        } else {
+          showLoad();
+        }
+      });
+    } catch (e) {
+      showError("invalid file");
+    }
+  };
+  reader.readAsText(fileInput.files[0]);
 }
 
 function addPage() {
@@ -16,8 +150,38 @@ function addPage() {
 }
 
 function removePage(p: number) {
-  // TODO: add confirm dialog
-  pages.splice(p, 1);
+  Swal.fire({
+    text: `are you sure you want to delete all the cards on page ${p + 1}?`,
+    title: "confirm delete",
+    icon: "warning",
+    showCancelButton: true,
+    cancelButtonText: "no, cancel",
+    cancelButtonColor: "#3085d6",
+    confirmButtonColor: "#d33",
+    confirmButtonText: "yes, delete",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      if (p === currentPage) {
+        if (p === 0) {
+          currentCard = 0;
+          currentPage = 0;
+        } else {
+          currentPage = p - 1;
+          for (let card of pages[currentPage]) {
+            if (card.title === "") {
+              currentCard = pages[currentPage].indexOf(card);
+              break;
+            }
+          }
+        }
+        if (p === endPage) {
+          endCard = 0;
+        }
+      }
+      pages.splice(p, 1);
+      render();
+    }
+  });
 }
 
 function setCard(p: number, c: number, card: Card) {
@@ -54,21 +218,163 @@ function getCard(p: number, c: number) {
   return pages[p][c];
 }
 
-function rerender() {
-  const rendered = template({ pages });
-  document.getElementById("app")!.innerHTML = rendered;
+function cardIsBlank(card: Card) {
+  return (
+    card.front === "" &&
+    card.back === "" &&
+    card.title === "" &&
+    card.corner === "" &&
+    card.index === ""
+  );
+}
+
+function findBlankCardOnPage(p: number) {
+  for (let c = 0; c < pages[p].length; c++) {
+    if (cardIsBlank(pages[p][c])) {
+      return c;
+    }
+  }
+  return -1;
+}
+
+function render() {
+  if (pages.length === 0) {
+    document.getElementById("app")!.innerHTML = `
+<div class="instructions">
+  <p>click edit to start creating flashcards</p>
+</div>
+`;
+    return;
+  }
+
+  document.getElementById("app")!.innerHTML = template({ pages });
+
   const editButtons = document.querySelectorAll(".edit-card-button");
   for (let button of editButtons) {
-    if (!(button instanceof HTMLElement)) {
+    if (
+      !(button instanceof HTMLElement) ||
+      button.dataset.cardIdx === undefined ||
+      button.dataset.cardIdx.length === 0 ||
+      button.dataset.pageNumber === undefined ||
+      button.dataset.pageNumber.length === 0
+    ) {
+      continue;
+    }
+    const buttonPN = Number(button.dataset.pageNumber);
+    const buttonCI = Number(button.dataset.cardIdx);
+    button.addEventListener("click", () => {
+      showEdit();
+      setFormToCard(buttonPN, buttonCI);
+    });
+  }
+
+  const deleteButtons = document.querySelectorAll(".del-card-button");
+  for (let button of deleteButtons) {
+    if (
+      !(button instanceof HTMLElement) ||
+      button.dataset.cardIdx === undefined ||
+      button.dataset.cardIdx.length === 0 ||
+      button.dataset.pageNumber === undefined ||
+      button.dataset.pageNumber.length === 0
+    ) {
       continue;
     }
     button.addEventListener("click", () => {
-      showEdit();
+      let text = `${Number(button.dataset.cardIdx) + 1}`;
+      if (button.dataset.cardText && button.dataset.cardText.length > 0) {
+        text = button.dataset.cardText;
+      }
+      Swal.fire({
+        text: `are you sure you want to erase all text on the card ${text}?`,
+        title: "confirm erase",
+        icon: "warning",
+        showCancelButton: true,
+        cancelButtonText: "no, cancel",
+        cancelButtonColor: "#3085d6",
+        confirmButtonColor: "#d33",
+        confirmButtonText: "yes, erase",
+      }).then((result) => {
+        setCard(
+          Number(button.dataset.pageNumber),
+          Number(button.dataset.cardIdx),
+          blankCard(),
+        );
+        if (result.isConfirmed) {
+          Swal.fire({
+            title: "Erased!",
+            text: "Card Erased",
+            icon: "success",
+          });
+          render();
+        }
+      });
       const buttonPN = Number(button.dataset.pageNumber);
       const buttonCI = Number(button.dataset.cardIdx);
       setFormToCard(buttonPN, buttonCI);
     });
   }
+
+  const cloneButtons = document.querySelectorAll(".dup-card-button");
+  for (let button of cloneButtons) {
+    if (
+      !(button instanceof HTMLElement) ||
+      button.dataset.cardIdx === undefined ||
+      button.dataset.cardIdx.length === 0 ||
+      button.dataset.pageNumber === undefined ||
+      button.dataset.pageNumber.length === 0
+    ) {
+      continue;
+    }
+    const cardToClone = getCard(
+      Number(button.dataset.pageNumber),
+      Number(button.dataset.cardIdx),
+    );
+    if (cardToClone === undefined) {
+      continue;
+    }
+    button.addEventListener("click", () => {
+      let idx = findBlankCardOnPage(Number(button.dataset.pageNumber));
+      let pageNum = Number(button.dataset.pageNumber);
+      let text = `${Number(button.dataset.cardIdx) + 1}`;
+      if (button.dataset.cardText && button.dataset.cardText.length > 0) {
+        text = button.dataset.cardText;
+      }
+      // no room on the same page
+      if (idx === -1) {
+        Swal.fire({
+          text: "there is no room on this page, would you like to clone to the last page?",
+          title: "confirm page",
+          icon: "warning",
+          showCancelButton: true,
+          cancelButtonText: "cancel",
+          cancelButtonColor: "#3085d6",
+          confirmButtonColor: "#d33",
+          confirmButtonText: "yes",
+        }).then((result) => {
+          if (result.isConfirmed) {
+            // Not yet on the last card on the page
+            if (endCard < 9) {
+              console.log("advance endCard");
+              endCard++;
+              pageNum = endPage;
+              idx = endCard;
+              // on the last card on the page
+            } else {
+              addPage();
+              endCard = 0;
+              endPage++;
+              idx = 0;
+              pageNum = endPage;
+            }
+            cloneCard(text, pageNum, idx, cardToClone);
+          }
+        });
+      } else {
+        cloneCard(text, pageNum, idx, cardToClone);
+      }
+    });
+  }
+
   const removepageButtons = document.querySelectorAll(".removepage");
   for (let button of removepageButtons) {
     if (!(button instanceof HTMLElement)) {
@@ -76,8 +382,51 @@ function rerender() {
     }
     button.addEventListener("click", () => {
       removePage(Number(button.dataset.pageNumber));
-      rerender();
+      render();
     });
+  }
+}
+
+function cloneCard(
+  text: string,
+  pageNum: number,
+  idx: number,
+  cardToClone: Card,
+) {
+  Swal.fire({
+    text: `are you sure you want to clone card ${text}?`,
+    title: "confirm clone",
+    icon: "question",
+    showCancelButton: true,
+    cancelButtonText: "no, cancel",
+    cancelButtonColor: "#3085d6",
+    confirmButtonColor: "green",
+    confirmButtonText: "yes, clone",
+  }).then((result) => {
+    setCard(pageNum, idx, cardToClone);
+    if (result.isConfirmed) {
+      Swal.fire({
+        title: "Cloned!",
+        text: "Card Cloned",
+        icon: "success",
+      });
+      render();
+    }
+  });
+}
+
+function showSave() {
+  const saveDialog = document.getElementById("save-dialog");
+  if (!(saveDialog instanceof HTMLDialogElement)) {
+    showError("save dialog not found");
+    return;
+  }
+  const saveNameInput = saveDialog.querySelector("#save-name");
+  saveDialog.showModal();
+  if (saveNameInput instanceof HTMLInputElement) {
+    const date = new Date();
+    saveNameInput.value = `${date.getFullYear()}${date.getMonth() + 1}${date.getDate()}-${date.getHours()}${date.getMinutes()}-cards`;
+    saveNameInput.focus();
   }
 }
 
@@ -90,8 +439,40 @@ function showEdit() {
   edit.querySelector("#card-number")!.textContent = `${currentCard + 1}`;
   edit.querySelector("#page-number")!.textContent = `${currentPage + 1}`;
   edit.showModal();
+  const frontTextField = edit.querySelector("#front");
+  if (frontTextField instanceof HTMLInputElement) {
+    frontTextField.focus();
+  }
   if (pages.length === 0) {
     addPage();
+  }
+}
+
+function showLoad() {
+  const loadDialog = document.getElementById("load-dialog");
+  if (!(loadDialog instanceof HTMLDialogElement)) {
+    showError("load dialog not found");
+    return;
+  }
+  const index: SavedIndexItem[] = JSON.parse(
+    localStorage.getItem("index") ?? "[]",
+  );
+  for (let item of index) {
+    item.date = new Date(item.date);
+  }
+  const loadTableBodyContent = loadTableTemplate({ index });
+  loadDialog.querySelector("#load-table-body")!.innerHTML =
+    loadTableBodyContent;
+  loadDialog.showModal();
+  const loadButtons = loadDialog.querySelectorAll(".load-item-button");
+  for (let button of loadButtons) {
+    if (!(button instanceof HTMLElement)) {
+      continue;
+    }
+    button.addEventListener("click", () => {
+      loadFromStorage(button.dataset.uniqueid ?? "");
+      closeLoad();
+    });
   }
 }
 
@@ -104,9 +485,27 @@ function closeEdit() {
   edit.close();
 }
 
+function closeSave() {
+  const saveDialog = document.getElementById("save-dialog");
+  if (!(saveDialog instanceof HTMLDialogElement)) {
+    showError("save dialog not found");
+    return;
+  }
+  saveDialog.close();
+}
+
+function closeLoad() {
+  const loadDialog = document.getElementById("load-dialog");
+  if (!(loadDialog instanceof HTMLDialogElement)) {
+    showError("load dialog not found");
+    return;
+  }
+  loadDialog.close();
+}
+
 function handleEdit(e: SubmitEvent) {
   e.preventDefault();
-  if (currentPage > endPage) {
+  if (currentPage > endPage || pages.length === 0) {
     addPage();
   }
   const target = e.target as HTMLFormElement;
@@ -129,7 +528,7 @@ function handleEdit(e: SubmitEvent) {
   } else {
     setFormToCard(currentPage, currentCard);
   }
-  rerender();
+  render();
 }
 
 export function setFormToCard(p: number, c: number) {
@@ -150,14 +549,51 @@ export function setFormToCard(p: number, c: number) {
     getCard(p, c)?.index ?? "";
   currentPage = p;
   currentCard = c;
+  document.getElementById("page-number")!.innerText = `${currentPage + 1}`;
+  document.getElementById("card-number")!.innerText = `${currentCard + 1}`;
 }
 
+// dialog controls
 document.getElementById("edit-button")?.addEventListener("click", showEdit);
 document.getElementById("close-edit")?.addEventListener("click", closeEdit);
+
+document.getElementById("save")?.addEventListener("click", function () {
+  showSave();
+});
+document.getElementById("close-save")?.addEventListener("click", closeSave);
+
+document.getElementById("load")?.addEventListener("click", function () {
+  showLoad();
+});
+document.getElementById("close-load")?.addEventListener("click", closeLoad);
+
 document.getElementById("print")?.addEventListener("click", function () {
   window.print();
 });
 document.getElementById("edit-form")!.addEventListener("submit", handleEdit);
+document
+  .getElementById("save-download")
+  ?.addEventListener("click", function () {
+    download();
+    closeSave();
+  });
+
+document.getElementById("save-browser")?.addEventListener("click", function () {
+  saveToBrowser();
+  closeSave();
+});
+
+document.getElementById("save-print")?.addEventListener("click", function () {
+  closeSave();
+  window.print();
+});
+
+document
+  .getElementById("load-from-file")
+  ?.addEventListener("submit", function (event) {
+    event.preventDefault();
+    loadFromFile();
+  });
 
 // debug code
 document.getElementById("testrender")?.addEventListener("click", function () {
@@ -173,7 +609,7 @@ document.getElementById("testrender")?.addEventListener("click", function () {
       index: `Index ${i}`,
     });
   }
-  rerender();
+  render();
 });
 
 document
